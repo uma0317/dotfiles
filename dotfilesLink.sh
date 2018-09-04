e68670491c45af0f1f77d7783c758be5725e4a15
uma0317 <blog3170@gmail.com>
2018-09-04 22:57:23 +0900

    add vscode keybindings

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,3 +2,4 @@ ln -sf ~/dotfiles/.zprofile ~/.zprofile
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sh ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -sh ~/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
