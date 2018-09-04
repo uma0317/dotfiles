e1b8509deb1874e74d0862f7af244e30230cf8c9
uma0317 <blog3170@gmail.com>
2018-09-04 23:03:55 +0900

    zprofileを同期しないようにした

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,4 +1,4 @@
-ln -sf ~/dotfiles/.zprofile ~/.zprofile
+# ln -sf ~/dotfiles/.zprofile ~/.zprofile
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sh ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
