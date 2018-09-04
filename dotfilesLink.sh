7bf6fa6a7d97f28f68d96b9370d7e9c6c7c8a6aa
uma0317 <blog3170@gmail.com>
2018-09-05 01:17:28 +0900

    マウス操作をオフにした

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,5 +1,5 @@
 ln -sf ~/dotfiles/zsh/.zprofile ~/.zprofile
 ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
-ln -sh ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
-ln -sh ~/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+ln -sf ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -sf ~/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
