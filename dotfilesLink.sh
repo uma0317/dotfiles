1888ec0a753493778e8f4856f6c0eb18459c7890
uma0317 <blog3170@gmail.com>
2019-08-01 19:38:51 +0900

    delete tmux dotfiles

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,5 +1,4 @@
 ln -sf ~/dotfiles/zsh/.zprofile ~/.zprofile
 ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
 ln -sf ~/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
