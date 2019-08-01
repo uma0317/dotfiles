ca37c3d674d27ab1158065d0b34423c04eeff085
uma0317 <blog3170@gmail.com>
2019-08-01 21:33:11 +0900

    fix

--- install.sh
+++ install.sh
@@ -1,17 +1,18 @@
 if type "apt-get" > /dev/null 2>&1; then
-    apt-get install zsh
+	apt-get update
+  apt-get install zsh
 fi
 
 if type "yum" > /dev/null 2>&1; then
-		yum install zsh
+	yum install zsh
 fi
 
 chsh -s $(which zsh)
+## zpluginインストール
 
 ln -sf zsh/.zprofile ~/.zprofile
 ln -sf zsh/.zshrc ~/.zshrc
 ln -sf vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
 ln -sf vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
 
-## zpluginインストールinstall
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zplugin/master/doc/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zplugin/master/doc/install.sh)"cat
