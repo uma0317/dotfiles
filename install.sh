1beb767e5d00c8a6c55876d5acc4f099660d4a67
uma0317 <blog3170@gmail.com>
2019-08-01 21:34:48 +0900

    fix

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
+if type "brew" > /dev/null 2>&1; then
+	brew install zsh
+fi
+
 if type "apt-get" > /dev/null 2>&1; then
 	apt-get update
   apt-get install zsh
@@ -15,4 +19,4 @@ ln -sf zsh/.zshrc ~/.zshrc
 ln -sf vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
 ln -sf vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zplugin/master/doc/install.sh)"cat
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zplugin/master/doc/install.sh)"
