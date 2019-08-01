d701d4b4964dbfe0e0460a7a752d4ac86cece5df
uma0317 <blog3170@gmail.com>
2019-08-01 20:04:04 +0900

    add zsh install code

--- dotfilesLink.sh
+++
@@ -1,4 +0,0 @@
-ln -sf ~/dotfiles/zsh/.zprofile ~/.zprofile
-ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
-ln -sf ~/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
