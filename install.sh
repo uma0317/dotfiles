1f67a83ce3976ef91538241fa6e2cb94ab8346ad
uma0317 <blog3170@gmail.com>
2019-08-01 20:17:27 +0900

    fix

--- install.sh
+++ install.sh
@@ -12,3 +12,6 @@ ln -sf zsh/.zprofile ~/.zprofile
 ln -sf zsh/.zshrc ~/.zshrc
 ln -sf vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
 ln -sf vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+
+## zpluginインストールinstall
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zplugin/master/doc/install.sh)"
