61ced0cdc9dd6d097c925f800494a16c1aac6131
uma0317 <blog3170@gmail.com>
2021-01-27 21:37:29 +0900

    add starship

--- install.sh
+++ install.sh
@@ -16,6 +16,7 @@ chsh -s $(which zsh)
 
 ln -sf ~/dotfiles/zsh/.zprofile ~/.zprofile
 ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/starship.toml ~/.config/starship.toml
 ln -sf ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
 ln -sf ~/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
 
